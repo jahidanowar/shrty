4f56b8d37d7eec20009bf9328365828eb89a7ed5
jahidanowar <milon5143@gmail.com>
2022-12-29 14:10:15 +0530

    fix: Redirecror

--- composables/useLinks.ts
+++ composables/useLinks.ts
@@ -1,4 +1,4 @@
-// import geoip from "geoip-lite";
+import geoip from "geoip-lite";
 import { Database } from "~~/types/supabase";
 
 export const useLinks = () => {
@@ -55,52 +55,9 @@ export const useLinks = () => {
     return res;
   };
 
-  const redirect = async (key: string) => {
-    const { data } = await client
-      .from("links")
-      .select("*")
-      .eq("key", key)
-      .single();
-    if (data) {
-      const clickData: any = {};
-
-      const ua = useUserAgent();
-
-      console.log({ ua });
-
-      if (ua) {
-        clickData.user_agent = ua.userAgent;
-        clickData.ip = ua.ip;
-
-        if (ua.ip) {
-          // const geo = geoip && geoip.lookup(ua.ip);
-          // if (geo) {
-          //   clickData.country = geo.country;
-          //   clickData.city = geo.city;
-          // }
-        }
-      }
-
-      const click = await client.from("clicks").insert({
-        link_id: data.id,
-        ...clickData,
-      });
-
-      console.log({ click });
-
-      useExternalRedirect(data.long_url, 302);
-    } else {
-      throw createError({
-        statusCode: 404,
-        message: "Link not found",
-      });
-    }
-  };
-
   return {
     getAllLinks,
     getLink,
     createLink,
-    redirect,
   };
 };
--- pages/[id].vue
+++ pages/[id].vue
@@ -1,6 +1,50 @@
 <script lang="ts" setup>
+import geoip from "geoip-lite";
+import { Database } from "~~/types/supabase";
+
+const client = useSupabaseClient<Database>();
 const params = useRoute().params;
-await useLinks().redirect(params.id as string);
+
+const { data } = await client
+  .from("links")
+  .select("*")
+  .eq("key", params.id)
+  .single();
+
+if (data) {
+  const clickData: any = {};
+
+  const ua = useUserAgent();
+
+  console.log({ ua });
+
+  if (ua) {
+    clickData.user_agent = ua.userAgent;
+    clickData.ip = ua.ip;
+
+    if (ua.ip) {
+      const geo = geoip.lookup(ua.ip);
+      if (geo) {
+        clickData.country = geo.country;
+        clickData.city = geo.city;
+      }
+    }
+  }
+
+  const click = await client.from("clicks").insert({
+    link_id: data.id,
+    ...clickData,
+  });
+
+  console.log({ click });
+
+  useExternalRedirect(data.long_url, 302);
+} else {
+  throw createError({
+    statusCode: 404,
+    message: "Link not found",
+  });
+}
 </script>
 
 <template>
